e5360754392fc5f8d31fd24815227469f60f030f
nashzerpinili <nashzerpinili@gmail.com>
2025-08-17 21:03:20 +0800

    Create code_challenge1.py

---
+++ code_challenge1.py
@@ -0,0 +1,2 @@
+x = input("Type your name-> ")
+print("\t\t\t\t\t* \n \t\t\t\t* \t\t* \n \t\t\t* \t\t\t\t* \n \t\t* \t\t\tHi! \t\t\t* \n \t* \t\t\t\tNashzer \t\t\t\t* \n \t\t* \t\t\t\t\t\t* \n \t\t\t* \t\t\t\t* \n \t\t\t\t* \t\t* \n \t\t\t\t\t*")
