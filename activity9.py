1e17cdface6c69be40bcbe23b66faf769cef2d78
nashzerpinili <nashzerpinili@gmail.com>
2025-08-24 23:17:05 +0800

    Create activity9.py

---
+++ activity9.py
@@ -0,0 +1,15 @@
+a = 3
+b = 2
+
+c = 6
+d = 5
+
+print (  ( a > b ) and ( c < d )  )
+print (  ( a < b ) or ( c < d )  )
+#print (  ( a < b ) not ( c < d )  )
+
+username = 'nashzer'
+password = 'pogiako123'
+
+print("User Login")
+print( (username == 'nashzer') and (password == 'pogiako123') )
