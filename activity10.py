27d3efac33b8ca9a96907b6518cd088d491b3723
nashzerpinili <nashzerpinili@gmail.com>
2025-09-07 19:50:35 +0800

    Create activity10.py

---
+++ activity10.py
@@ -0,0 +1,10 @@
+username = 'nashzer'
+password = 'nash'
+
+u = input("USERNAME --> ")
+p = input("PASSWORD --> ")
+
+if (u == username) and (p == password):
+    print("Access granted")
+else:
+    print("Access denied")
