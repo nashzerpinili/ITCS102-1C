3e23dd85ae46316880eb7bc742d873536f35969b
nashzerpinili <nashzerpinili@gmail.com>
2025-09-30 13:10:34 +0800

    Create activity19.py

---
+++ activity19.py
@@ -0,0 +1,18 @@
+print("\t\t *", end=" ")
+for e in range(1,11,1):
+    for f in range(10,e,-1):
+        print(" ", end=" ")
+    for g in range(1,e,1):
+        print("*", end=" ")
+    for h in range(1,e,1):
+        print("*", end=" ")
+    print()
+
+for e in range(1,11,1):
+    for f in range(10,e,-1):
+        print("*", end=" ")
+    for g in range(10,e,-1):
+        print("*", end=" ")
+    for h in range(1,11,1):
+        print(" ", end=" ")
+    print()
