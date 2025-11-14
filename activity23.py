4d6c8e2d3418411ca5456fedcf5555d5dd20f901
nashzerpinili <nashzerpinili@gmail.com>
2025-11-14 22:16:19 +0800

    Add activity23.py with greeting functions and prints

---
+++ activity23.py
@@ -0,0 +1,13 @@
+from activity23_1 import Greetwithname, GreetPerson, FunctionWithReturn, FactorialWithReturn
+
+def MyFunction():
+    print("HI VLOG")
+    print("WELCOME TO MY GUYS")
+
+MyFunction()
+Greetwithname("Bronny")
+Greetwithname("JAMES")
+GreetPerson("Enteng", "Iyam", "23")
+
+print(f"I want to get summation of {FunctionWithReturn(8)}")
+print(f"I want to get summation of {FunctionWithReturn(10) + 25}")
