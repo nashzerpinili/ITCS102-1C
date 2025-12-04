5dd43472dfa03a53f53e9e1b2084b67421256682
nashzerpinili <nashzerpinili@gmail.com>
2025-12-04 13:25:03 +0800

    Add programming language list and dictionary example

---
+++ activity25.py
@@ -0,0 +1,14 @@
+#list
+
+programming_language = ['java' , 'C#' , 'C++' , 'Perl' , 'Python' , 'JavaScript' , 'Rust' , 'GoLang']
+
+print(programming_language[-1])
+
+#dictionary
+
+empty_dictionary = {}
+
+
+p1_dicionary = {'medyo mahirap':'jaya','tama lang':'C#','mahirap talaga':'C++','luma na':'C','pang matanda':'Perl','ok lang':'Python','mahirap din':'JavaScript','di ko alam':'Rust','bago':'GoLang'}
+
+print(p1_dicionary['ok lang'])
